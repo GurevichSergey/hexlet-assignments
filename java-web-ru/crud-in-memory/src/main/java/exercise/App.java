409243e52cd336432dc0e83a014ba94007a06114
@hexlet/cli <support@hexlet.io>
2023-03-27 23:48:07 +1000

    submit java-web-ru/crud-in-memory

--- java-web-ru/crud-in-memory/src/main/java/exercise/App.java
+++ java-web-ru/crud-in-memory/src/main/java/exercise/App.java
@@ -15,7 +15,7 @@ public class App {
         if (port != null) {
             return Integer.valueOf(port);
         }
-        return 5000;
+        return 8000;
     }
 
     public static Tomcat getApp(int port) {
