65295c6f9407bef03a95d016cc8088c040c3c321
@hexlet/cli <support@hexlet.io>
2023-03-27 23:43:34 +1000

    submit java-web-ru/crud-in-memory

--- java-web-ru/crud-in-memory/src/main/java/exercise/App.java
+++ java-web-ru/crud-in-memory/src/main/java/exercise/App.java
@@ -18,22 +18,22 @@ public class App {
         return 5000;
     }
 
-//    public static Tomcat getApp(int port) {
-//        Tomcat tomcat = new Tomcat();
-//
-//        tomcat.setBaseDir(System.getProperty("java.io.tmpdir"));
-//        tomcat.setPort(port);
-//
-//        Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
-//
-//        tomcat.addServlet(ctx, WelcomeServlet.class.getSimpleName(), new WelcomeServlet());
-//        ctx.addServletMappingDecoded("", WelcomeServlet.class.getSimpleName());
-//
+    public static Tomcat getApp(int port) {
+        Tomcat tomcat = new Tomcat();
+
+        tomcat.setBaseDir(System.getProperty("java.io.tmpdir"));
+        tomcat.setPort(port);
+
+        Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
+
+        tomcat.addServlet(ctx, WelcomeServlet.class.getSimpleName(), new WelcomeServlet());
+        ctx.addServletMappingDecoded("", WelcomeServlet.class.getSimpleName());
+
 //        tomcat.addServlet(ctx, UsersServlet.class.getSimpleName(), new UsersServlet());
 //        ctx.addServletMappingDecoded("/users/*", UsersServlet.class.getSimpleName());
-//
-//        return tomcat;
-//    }
+
+        return tomcat;
+    }
 
     public static void main(String[] args) throws LifecycleException {
         Tomcat app = getApp(getPort());
