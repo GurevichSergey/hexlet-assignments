bd3119351f1d5cca99b69b825cd4c3f0653e97e1
@hexlet/cli <support@hexlet.io>
2023-05-08 02:16:19 +1000

    submit java-spring-ru/custom-annotations

---
+++ java-spring-ru/custom-annotations/src/main/java/exercise/CustomBeanPostProcessor.java
@@ -0,0 +1,57 @@
+package exercise;
+
+import java.lang.reflect.Proxy;
+
+import exercise.calculator.Calculator;
+import exercise.calculator.CalculatorImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// BEGIN
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomBeanPostProcessor.class);
+    private Map<String, String> expectedBean = new HashMap<>();
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean.getClass().isAnnotationPresent(Inspect.class)) {
+            String level = bean.getClass().getAnnotation(Inspect.class).level();
+            expectedBean.put(beanName, level);
+        }
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (expectedBean.containsKey(beanName)) {
+            return  Proxy.newProxyInstance(
+                    bean.getClass().getClassLoader(),
+                    bean.getClass().getInterfaces(),
+                    (proxy, method, args) -> {
+                        if (method.getName().equals("sum") || method.getName().equals("mult")) {
+                            String message = "Was called method: " + method.getName() + "() with arguments: " + Arrays.toString(args);
+                            if (expectedBean.get(beanName).equals("info")) {
+
+                                LOGGER.info(message);
+                            } else {
+                                LOGGER.debug(message);
+                            }
+                        } else {
+                            throw new UnsupportedOperationException(
+                                    "Unsupported operation: " + method.getName()
+                            );
+                        }
+                        return method.invoke(bean, args);
+                    }
+            );
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
+// END
